27d8bc3f463e444ed8bca5b607aaf7f09b53efab
Nightscrawl <52441295+Nightscrawl@users.noreply.github.com>
2020-01-12 14:27:14 -0800

    First commit
    Includes php error test and dummy index.html.

---
+++ web120/error.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta charset="utf-8">
+        <title> ... </title>
+    </head>
+    
+    <body>
+    
+        <?php
+        ech 'This is PHP!';
+    
+    </body>
+    
+</html>
