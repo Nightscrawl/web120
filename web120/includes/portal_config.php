09dec9e966739d01ea3af419d310bcd9af132c37
Nightscrawl <52441295+Nightscrawl@users.noreply.github.com>
2020-01-23 17:21:49 -0800

    Bio update with new full commit
    Updates the welcome page bio.

--- web120/includes/portal_config.php
+++ web120/includes/portal_config.php
@@ -64,7 +64,7 @@ $nav1['index.php'] = "Welcome";
 $nav1['big/index.php'] = "Big";
 $nav1['aia.php'] = "AIA";
 $nav1['flowchart.php'] = "Flowchart";
-$nav1['fp/index.php'] = "Final Project";
+$nav1['fp/index.html'] = "Final Project";
 $nav1['contact.php'] = "Contact";
 
 
--- web120/index.php
+++ web120/index.php
@@ -5,23 +5,23 @@
                     <h3>Hello!</h3>
 
                     <p>
-                        My name is Kira and this is my second year at Seattle Central College. I started off on the database track, but taking Web 110 last quarter reminded me of my interest in web development and design, so I have switched my track to that path. I am hoping to complete my programming associate's degree some time in 2021. In addition, I am a member of the <a href="https://sites.google.com/view/scptk/home" target="_blank">Phi Theta Kappa</a> honor society at SCC.
+                        I’ve travelled over four-thousand miles to get to where I am. Born and raised in Miami, Florida, eleven years ago, I undertook a two-month car journey to Seattle, Washington, taking in the various sights and sites along the way. To this day, it remains my greatest adventure.
                     </p>
 
                     <p>
-                        I have an interest in working with non-profit, philanthropic, charitable organizations (which can take many different forms!), which is what I am ultimately hoping to do with my degree and the skills I develop while working toward it. In the 21st century, it is important for organizations of all sorts to have a viable web presence; this is not limited only to for-profit businesses; non-profits need to have it as well in order to spread awareness of their cause, maximize reach, and obtain the donations they need to continue with their good and important work in our communities here at home and around the world.
+                        Now, I’m on a different sort of journey, one of self-improvement as I pursue a degree in the field of web development. I have an interest in working with non-profit, philanthropic, charitable organizations, which is what I am ultimately hoping to do with my degree and the skills I develop while working toward it. In the 21st century, it is important for organizations of all sorts to have a viable web presence; this is not limited only to for-profit businesses; non-profits need to have it as well in order to spread awareness of their cause, maximize reach, and obtain the donations they need to continue their good and important work.
                     </p>
 
                     <p>
-                        On a more personal note, while my played library isn't very extensive, I consider myself to be a gamer. I used to be an active player of <i>World of Warcraft</i>, which I played for about 10 years. In addition, I was an officer in a few different guilds, as well as a raid leader. Helping to organize a bunch of people (up to 25!) is no easy task! While I've been out of that scene for a few years, I'm hoping that experience will serve me well in the years to come.
+                        On a more casual note, while my played library isn't very extensive, I consider myself to be a gamer. I used to be an active player of <i>World of Warcraft</i>, which I played for about ten years. In addition, I was an officer in a few different guilds, as well as a raid leader. Helping to organize a bunch of people (up to 25!) is no easy task! While I've been out of that scene for a few years, I'm hoping that experience will serve me well in the years to come.
                     </p>
 
                     <p>
-                        My primary game genre is singleplayer RPG, and more specifically <i>Skyrim</i> and Dragon Age. I am an avid modder of both of those games, both in use and mod creation. I also write a bit of fanfic concerning my character from <i>Dragon Age: Inquisition</i>.
+                        My primary game genre is singleplayer RPG, and more specifically Dragon Age and <i>Skyrim</i>. I am an avid mod user and creator for both games. I also write a bit of fanfic concerning my character from <i>Dragon Age: Inquisition</i>.
                     </p>
 
                     <p>
-                        Finally, I try to maintain my fitness through running and am currently trying to increase my endurance. With three classes this quarter, I have a bit of worry that I won't have as much time to do some running, but I'll try to get in here and there. This current weather isn't helping! ;)
+                        Finally, I try to maintain my fitness through running, both indoors and out, when the weather permits. Flying down Seattle’s hills is exhilarating until you have to go back up again!
                     </p>
 
                 </article>
